06c513d978dbfccfddeefbb174c94ccb0a61823d
noj33 <jonfriese33@gmail.com>
2013-08-21 21:42:24 -0700

    New library, 3 classes.

--- library.rb
+++ library.rb
@@ -1,58 +1,48 @@
-class Book
-	def initialize(title, author = '?', year = '?')
-		@title = title
-		@author = author
-		@year = year
-	end
-	def book_description
-		"\n#{@title} was written by #{@author} in #{@year}."
-	end
-end
-
 class Library
-	attr_reader :shelf_1, :shelf_2, :shelf_3
-	def initialize
-		@shelf_1 = []
-		@shelf_2 = []
-		@shelf_3 = []
-	end
-	def enshelf_1(book)
-		shelf_1 << book
-	end	
-	def enshelf_2(book)
-		shelf_2 << book
-	end
-	def enshelf_3(book)
-		shelf_3 << book	
-	end
-	def unshelf_1(book)
-		shelf_1.delete(book)
-	end
-	def unshelf_2(book)
-		shelf_2.delete(book)
-	end
-	def unshelf_3(book)
-		shelf_3.delete(book)
-	end
-	def all_books
-		"\nShelf 1: #{shelf_1}  Shelf 2: #{shelf_2} Shelf_3: #{shelf_3}"
-	end	
+    @@shelves = nil
+    
+    def initialize
+        @@shelves = [[],[],[]]
+    end
+    
+    def add_shelf
+        @@shelves.push([])
+    end
+    
+    def print_library
+    	p @@shelves
+    end   
 end
 
+class Shelves < Library
+    def print_shelf(x)
+    	x -= 1
+    	p @@shelves[x]
+    end
+end
 
-
-
-wpGtR = Book.new("why's (poignant) Guide to Ruby","why the lucky stiff",2005)
-library = Library.new
-p library.shelf_1
-WtRFG = Book.new('Where the Red Fern Grows')
-puts WtRFG.book_description
-library.enshelf_1("wpGtR")
-p library.shelf_1
-puts library.all_books
-puts wpGtR.book_description
-library.enshelf_2("WtRFG")
-puts library.all_books
-library.unshelf_1("wpGtR")
-library.enshelf_2("wpGtR")
-puts library.all_books
+class Book < Shelves
+    def enshelf(a , x)
+	x -= 1
+	@@shelves[x].push(a)
+    end
+	
+    def unshelf(a , x)
+	x -= 1
+	@@shelves[x].delete(a)
+    end
+end
+		
+x = Library.new
+x.add_shelf
+t = Shelves.new
+t.print_shelf(1)
+new_book = Book.new
+new_book.enshelf("James and the Giant Peach" , 1)
+t.print_shelf(1)
+new_book.unshelf("James and the Giant Peach" , 1)
+t.print_shelf(1)
+x.add_shelf
+new_book.enshelf("Where the Red Fern Grows", 4)
+t.print_shelf(4)
+x.print_library
